12779cea3d712df3e211097037d8140ac0379261
Scott Kao <scottxxx666@gmail.com>
2020-03-17 02:38:54 +0800

    [feat] handle ticket input change

--- src/TicketCreate.js
+++ src/TicketCreate.js
@@ -7,6 +7,7 @@ import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
+import {useState} from 'react';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -18,6 +19,15 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export default function () {
+  function handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    setTicket(prev => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
+  const [ticket, setTicket] = useState();
   const classes = useStyles();
   return (
     <Grid container justify="center">
@@ -30,34 +40,73 @@ export default function () {
               </Typography>
             </Grid>
             <Grid item xs={12} className={classes.row}>
-              <TextField id="area" label="票卷區域" placeholder="紅2A區" margin="normal" required/>
+              <TextField
+                id="area"
+                name="area"
+                label="票卷區域"
+                placeholder="紅2A區"
+                margin="normal"
+                onChange={handleChange}
+                required
+              />
             </Grid>
             <Grid item xs={12} className={classes.row}>
-              <TextField id="seat" label="座位序號" placeholder="10排12號" margin="normal" required/>
+              <TextField
+                id="seat"
+                name="seat"
+                label="座位序號"
+                placeholder="10排12號"
+                margin="normal"
+                onChange={handleChange}
+                required
+              />
             </Grid>
             <Grid item xs={12} className={classes.row}>
-              <TextField id="number"
-                         label="張數"
-                         type="number"
-                         defaultValue="1"
-                         margin="normal"
-                         inputProps={{ min: 1 }}
-                         required/>
+              <TextField
+                id="number"
+                name="number"
+                label="張數"
+                type="number"
+                defaultValue="1"
+                margin="normal"
+                inputProps={{ min: 1 }}
+                onChange={handleChange}
+                required
+              />
             </Grid>
             <Grid item xs={12} className={classes.row}>
-              <TextField id="price"
-                         label="預售價格"
-                         type="number"
-                         helperText="注意：售價不得比原票價高！"
-                         margin="normal"
-                         inputProps={{ min: 0 }}
-                         required/>
+              <TextField
+                id="price"
+                name="price"
+                label="預售價格"
+                type="number"
+                helperText="注意：售價不得比原票價高！"
+                margin="normal"
+                inputProps={{ min: 0 }}
+                onChange={handleChange}
+                required
+              />
             </Grid>
             <Grid item xs={12} className={classes.row}>
-              <TextField id="payment" label="交易方式" margin="normal" multiline required/>
+              <TextField
+                id="payment"
+                name="payment"
+                label="交易方式"
+                margin="normal"
+                multiline
+                onChange={handleChange}
+                required
+              />
             </Grid>
             <Grid item xs={12} className={classes.row}>
-              <TextField id="note" label="備註" multiline margin="normal"/>
+              <TextField
+                id="note"
+                name="note"
+                label="備註"
+                multiline
+                margin="normal"
+                onChange={handleChange}
+              />
             </Grid>
             <Grid item xs={12} className={classes.row}>
               <TextField id="ptt_id" label="PTT ID" margin="normal"/>
