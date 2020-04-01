9ba0b58e4f3f822d7334fbf1f5edccb53030ec4f
Scott Kao <scottxxx666@gmail.com>
2020-04-01 23:45:59 +0800

    [feat] redirect after login success

--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,7 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import {makeStyles} from '@material-ui/core/styles';
 import AuthContext from './AuthContext';
-import {useHistory} from 'react-router-dom';
+import {useHistory, useLocation} from 'react-router-dom';
 import {useMutation} from '@apollo/react-hooks';
 import {gql} from 'apollo-boost';
 
@@ -26,6 +26,10 @@ const LOGIN_MUTATION = gql`
     }
 `;
 
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
 export default () => {
   const [login] = useMutation(LOGIN_MUTATION, {
     onCompleted: (data) => {
@@ -37,10 +41,14 @@ export default () => {
   const auth = useContext(AuthContext);
   const successHandler = (data) => {
     login({ variables: { token: data.tokenId } });
+    history.push(url);
   };
   const failureHandler = (e) => {
     console.error(e);
   };
+
+  const query = useQuery();
+  const url = query.get('url');
 
   const classes = useStyles();
   const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
