9c0f62d5641386f431efa7e8439bb4573f196aa9
Scott Kao <scottxxx666@gmail.com>
2020-04-04 02:25:24 +0800

    [feat] login if click account circle

--- src/AccountCircle.js
+++ src/AccountCircle.js
@@ -1,11 +1,14 @@
 import AccountCircleIcon from '@material-ui/icons/AccountCircleRounded';
 import IconButton from '@material-ui/core/IconButton';
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default () => {
   return (
-    <IconButton>
-      <AccountCircleIcon fontSize="large"/>
-    </IconButton>
+    <Link to={location => ({ pathname: '/login', state: { from: location } })}>
+      <IconButton>
+        <AccountCircleIcon fontSize="large"/>
+      </IconButton>
+    </Link>
   );
 };
