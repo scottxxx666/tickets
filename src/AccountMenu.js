263e6cf12be3dc1befd18ab1e93fa09b09ede725
Scott Kao <scottxxx666@gmail.com>
2020-04-04 01:18:41 +0800

    [refactor] extract account menu

---
+++ src/AccountMenu.js
@@ -0,0 +1,57 @@
+import Button from '@material-ui/core/Button';
+import AccountCircleIcon from '@material-ui/icons/AccountCircleRounded';
+import Popper from '@material-ui/core/Popper';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import MenuList from '@material-ui/core/MenuList';
+import MenuItem from '@material-ui/core/MenuItem';
+import React, {useContext, useState} from 'react';
+import AuthContext from './AuthContext';
+
+const AccountMenu = () => {
+  const [open, setOpen] = useState(false);
+  const toggleMenu = () => {
+    setOpen((prev) => !prev);
+  };
+
+  const anchorRef = React.useRef(null);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const logout = () => {
+    auth.logout();
+  };
+  const auth = useContext(AuthContext);
+
+  return (
+    <React.Fragment>
+      <Button
+        edge="end"
+        variant="contained"
+        color="default"
+        endIcon={<AccountCircleIcon/>}
+        onClick={toggleMenu}
+        ref={anchorRef}
+      >nema</Button>
+      <Popper open={open} anchorEl={anchorRef.current} transition disablePortal>
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList>
+                  <MenuItem onClick={logout}>Logout</MenuItem>
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </React.Fragment>
+  );
+};
+
+export default AccountMenu;
--- src/AppTopBar.js
+++ src/AppTopBar.js
@@ -1,19 +1,13 @@
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
 import AccountCircleIcon from '@material-ui/icons/AccountCircleRounded';
 import IconButton from '@material-ui/core/IconButton';
 import React, {Fragment, useContext, useState} from 'react';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import Slide from '@material-ui/core/Slide';
 import makeStyles from '@material-ui/core/styles/makeStyles';
-import MenuList from '@material-ui/core/MenuList';
-import MenuItem from '@material-ui/core/MenuItem';
-import Paper from '@material-ui/core/Paper';
-import Popper from '@material-ui/core/Popper';
-import Grow from '@material-ui/core/Grow';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import AuthContext from './AuthContext';
+import AccountMenu from './AccountMenu';
 
 const useStyles = makeStyles((theme) => ({
   grow: {
@@ -33,53 +27,18 @@ function HideOnScroll(props) {
 
 const AppTopBar = () => {
   const classes = useStyles();
-  const [open, setOpen] = useState(false);
-  const toggleMenu = () => {
-    setOpen((prev) => !prev);
-  };
-
-  const anchorRef = React.useRef(null);
-  const handleClose = () => {
-    setOpen(false);
-  };
-
   const auth = useContext(AuthContext);
-  const logout = () => {
-    auth.logout();
-  };
+
   return (
     <Fragment>
       <HideOnScroll>
         <AppBar position="fixed">
           <Toolbar>
             <div className={classes.grow}/>
-            <Button
-              edge="end"
-              variant="contained"
-              color="default"
-              endIcon={<AccountCircleIcon/>}
-              onClick={toggleMenu}
-              ref={anchorRef}
-            >{'nema'}</Button>
-            <Popper open={open} anchorEl={anchorRef.current} transition disablePortal>
-              {({ TransitionProps, placement }) => (
-                <Grow
-                  {...TransitionProps}
-                  style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
-                >
-                  <Paper>
-                    <ClickAwayListener onClickAway={handleClose}>
-                      <MenuList>
-                        <MenuItem onClick={logout}>Logout</MenuItem>
-                      </MenuList>
-                    </ClickAwayListener>
-                  </Paper>
-                </Grow>
-              )}
-            </Popper>
             <IconButton>
               <AccountCircleIcon fontSize="large"/>
             </IconButton>
+                <AccountMenu/>
           </Toolbar>
         </AppBar>
       </HideOnScroll>
