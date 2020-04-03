560d39b66341e1673d2805f1e18d053d0d0eee5e
Scott Kao <scottxxx666@gmail.com>
2020-04-04 02:40:19 +0800

    [style] remove unused import

--- src/AppTopBar.js
+++ src/AppTopBar.js
@@ -1,8 +1,6 @@
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import AccountCircleIcon from '@material-ui/icons/AccountCircleRounded';
-import IconButton from '@material-ui/core/IconButton';
-import React, {Fragment, useContext, useState} from 'react';
+import React, {Fragment, useContext} from 'react';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import Slide from '@material-ui/core/Slide';
 import makeStyles from '@material-ui/core/styles/makeStyles';
