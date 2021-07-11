80772cf05bae35ac269a70a62e7a5f8abfb704d1
Scott Kao <scottxxx666@gmail.com>
2021-07-11 14:54:26 +0800

    [test] add build to github pages

---
+++ build/precache-manifest.864577213d1227d84fd10f60d2fa50e0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1fc80e1bb4a793516b2f085741c45ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "174f0c94854266fa2deb",
+    "url": "/static/js/2.98d103e5.chunk.js"
+  },
+  {
+    "revision": "888f1eb2950970f6b72bd79ac072d9b4",
+    "url": "/static/js/2.98d103e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22f341f4e0c738c3ee5c",
+    "url": "/static/js/3.33fae91c.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/3.33fae91c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33b67f600cfb25af563c",
+    "url": "/static/js/4.4722abba.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/static/js/4.4722abba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50146c91f50ccaa73515",
+    "url": "/static/js/5.cddcc74f.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/static/js/5.cddcc74f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e67149a5789bf706b773",
+    "url": "/static/js/main.30aa4a61.chunk.js"
+  },
+  {
+    "revision": "d425f7f3b94732ea1305",
+    "url": "/static/js/runtime-main.2e2e98b6.js"
+  }
+]);
